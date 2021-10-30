604ee39d55302c6b33b89e0a51f17b85239d8f5e
Umer Yasin <40846948+UmerYasin1@users.noreply.github.com>
2021-10-31 00:02:01 +0500

    Add Components

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import head from './App';
+import nav from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <head />
+    <nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
